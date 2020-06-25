6ccedf3cf5a87177248550ed4a8d1c6e6f30d9b5
Codefred <alfredemmanuelinyang@gmail.com>
2020-06-25 04:47:34 +0100

    deploy change

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ def webhook():
 
 
 
-print("bot polling...")
-bot.remove_webhook()
-bot.polling(none_stop=True)
+# print("bot polling...")
+# bot.remove_webhook()
+# bot.polling(none_stop=True)
 
-# if __name__ == "__main__":
-#     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 
 
