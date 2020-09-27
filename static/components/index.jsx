01d49d200e07055fbff8e7354bf0d41cdc55823e
Codefred <alfredemmanuelinyang@gmail.com>
2020-09-27 22:44:26 +0100

    react running

---
+++ static/components/index.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+class Dashboard extends React.Component {
+
+    render() { 
+        return (
+            <section>
+                <p>React Component Is Active 🤯</p>
+            </section>
+        );
+    }
+
+};
+
+
+ReactDOM.render(<Dashboard />, document.getElementById('root'));
